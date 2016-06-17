d5b2bb622649ba34de5b85b681dea531a4c97329
superpierre <pierre.hoezelle>
2016-06-17 13:26:33 +0200

    test for save titrage

--- modules/titreur.js
+++ modules/titreur.js
@@ -4,6 +4,7 @@ function Titreur ( module ) {
 
 	// attach external process
 	module.setProcess('./modules/bin/hardware6');
+	var fs = require('fs');
 
 	// set Titreur
 	module.extends('titreur', (data) => {
@@ -13,6 +14,8 @@ function Titreur ( module ) {
 	        if (data.line1) cmd += ' -line1 ' + data.line1.replace(/ /g, '_');
 	        if (data.line2) cmd += ' -line2 ' + data.line2.replace(/ /g, '_');
 			module.write(cmd);
+			fs.appendFile('/dnc/media/titrage.txt', data.line1 + ' — ' + data.line2 + '\n\r', function (err) { });
+
 			//console.log('MODULE Titreur: '+cmd);
 		})
 		.describe('Titreur', {line1: 'text', line2: 'text'});
@@ -24,6 +27,8 @@ function Titreur ( module ) {
 		        if (data.line1b) cmd += ' -line1 ' + data.line1b.replace(/ /g, '_');
 				module.write(cmd);
 				//console.log('MODULE Titreur: '+cmd);
+				fs.appendFile('/dnc/media/titrage.txt', 'B' + data.line1b + '\n\r', function (err) { });
+
 			})
 			.describe('Titreur BIG', {line1b: 'text'});
 
